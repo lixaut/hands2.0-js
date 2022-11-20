fb2aa4f7d8d4bd146548a7400292f1d63d69a54f
lixaut <wanglixaut@qq.com>
2022-11-20 11:52:14 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 
+class MyPromise {
+    constructor(executor) {
+      executor(this.resolve, this.reject);
+      this.status = 'pending';
+      this.value = null;
+      this.reason = null;
+    }
+  
+    resolve(value) {
+      if (this.status === 'pending') {
+        this.status = 'fulfilled';
+        this.value = value;
+      }
+    }
+  
+    reject(reason) {
+      if (this.status === 'pending') {
+        this.status = 'rejected';
+        this.reason = reason;
+      }
+    }
+  
+    then(onFulfilled, onRejected) {
+      if (this.status === 'fulfilled') {
+        onFulfilled(this.value);
+      } else if (this.status === 'rejected') {
+        onRejected(this.reason);
+      }
+    }
+  }
+  
+  const promise = new MyPromise((resolve, reject) => {
+    resolve('success');
+    reject('err');
+  });
+  
+  console.log(promise);
+  // promise.then(value => {
+  //     console.log('value', value)
+  // }, reason => {
+  //     console.log('reject', reason)
+  // })
+  
