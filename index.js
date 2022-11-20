e6475394c62ceb77b726b25b0e3c5e3b0da137a0
lixaut <wanglixaut@qq.com>
2022-11-20 12:41:32 +0800

    arrowfun

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
 
 class MyPromise {
-    constructor(executor) {
-      executor(this.resolve, this.reject);
-      this.status = 'pending';
-      this.value = null;
-      this.reason = null;
-    }
-  
-    resolve(value) {
-      if (this.status === 'pending') {
-        this.status = 'fulfilled';
-        this.value = value;
-      }
-    }
-  
-    reject(reason) {
-      if (this.status === 'pending') {
-        this.status = 'rejected';
-        this.reason = reason;
-      }
-    }
-  
-    then(onFulfilled, onRejected) {
-      if (this.status === 'fulfilled') {
-        onFulfilled(this.value);
-      } else if (this.status === 'rejected') {
-        onRejected(this.reason);
-      }
+  constructor(executor) {
+    this.status = 'pending';
+    this.value = null;
+    this.reason = null;
+    executor(this.resolve, this.reject);
+  }
+
+  resolve = (value) => {
+    if (this.status !== 'pending') return
+    this.status = 'fulfilled';
+    this.value = value;
+  }
+
+  reject = (reason) => {
+    if (this.status !== 'pending') return
+    this.status = 'rejected';
+    this.reason = reason;
+  }
+
+  then(onFulfilled, onRejected) {
+    if (this.status === 'fulfilled') {
+      onFulfilled(this.value);
+    } else if (this.status === 'rejected') {
+      onRejected(this.reason);
     }
   }
-  
-  const promise = new MyPromise((resolve, reject) => {
-    resolve('success');
-    reject('err');
-  });
-  
-  console.log(promise);
-  // promise.then(value => {
-  //     console.log('value', value)
-  // }, reason => {
-  //     console.log('reject', reason)
-  // })
-  
+}
+
+const promise = new MyPromise((resolve, reject) => {
+  resolve('success');
+  reject('err');
+});
+promise.then(value => {
+    console.log('value', value)
+}, reason => {
+    console.log('reject', reason)
+})
